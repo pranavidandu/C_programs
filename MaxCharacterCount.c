fddbaa94ca0199b93300c88f9330be4dc990f9fe
pranavi <dandupranavi@gmail.com>
2015-11-06 11:25:16 +0530

    programs updated

--- MaxCharacterCount.c
+++ MaxCharacterCount.c
@@ -1,25 +1,23 @@
 #include <stdio.h>
-#include <stdlib.h>
 void max_character_count(char []);
 int main(){
-	char given_array[20] = "abaacbbboopuuuuu";
+	char given_array[20];
+	printf("Enter a string\n");
+	scanf("%s", given_array);
 	max_character_count(given_array);
-	_getch();
 	return 0;
 }
-void max_character_count(char given_array[10]){
+void max_character_count(char given_array[]){
 	int count_characters[26] = { 0 };
-	int max_character_index = 0;
+	char max_character_index;
+        int max_count = 0;
 	for (int i = 0; given_array[i] != '\0'; i++){
 		count_characters[given_array[i] - 97]++;
-	}
-	int max_count = count_characters[0];
-	for (int i = 1; i < 26; i++){
-		if (count_characters[i] > max_count){
-			max_count = count_characters[i];
-			max_character_index = i;
+		if (count_characters[given_array[i] - 97] > max_count){
+			max_count = count_characters[given_array[i] - 97];
+			max_character_index = given_array[i];
 		}
 	}
-	printf("%c: %d\n",97 + max_character_index, max_count);
+	printf("%c: %d\n",max_character_index, max_count);
 	return;
 }
--- common_characters.c
+++ common_characters.c
@@ -1,33 +1,35 @@
 #include <stdio.h>
-void common_chars(char[], char[]);
+void common_characters(char[], char[]);
 int main(){
-	char given_array1[10] = "aaabccfgt";
-	char given_array2[10] = "abcat";
-	common_chars(given_array1, given_array2);
-	_getch();
+	char given_array1[20];
+	printf("Enter first string\n");
+	scanf("%s", given_array1);
+	char given_array2[20];
+	printf("Enter second string\n");
+	scanf("%s", given_array2);
+	common_characters(given_array1, given_array2);
 	return 0;
 }
-void common_chars(char given_array1[10], char given_array2[10]){
+void common_characters(char given_array1[10], char given_array2[10]){
 	int common_characters[26] = { 0 };
-	int repeat_characters[26] = { 0 };
-	for (int i = 0; given_array1[i] != '\0'; i++){	
-		repeat_characters[given_array1[i] - 97]++;
-		if (repeat_characters[given_array1[i] - 97] > 1)
+	int i, flag = 0;
+	for(i = 0; given_array1[i] != '\0'; i++){
+		if(common_characters[given_array1[i] - 97] == 1)
 			continue;
-		for (int j = 0; given_array2[j] != '\0'; j++){
-			if (given_array1[i] == given_array2[j]){
-				printf("i is %d\n", i);
-				common_characters[given_array1[i] - 97]++;
-			}
-		}
+		common_characters[given_array1[i] - 97]++;
 	}
-	for (int i = 0; i < 26; i++)
-		printf("%d", common_characters[i]);
-	for (int i = 0; i < 26; i++){
-		if (common_characters[i] > 0){
-			printf("%c", 97 + i);
-			
-		}
+	for(i = 0; given_array2[i] != '\0'; i++){
+		if(common_characters[given_array2[i] - 97] == 2 || common_characters[given_array2[i] - 97] == 0)
+			continue;
+		common_characters[given_array2[i] - 97]++;
+	}	
+	printf("Common Characters:");
+	for(i = 0; i < 26; i++){
+		if(common_characters[i] == 2){
+			flag = 1;			
+			printf(" %c", 97 + i);}
 	}
-	return;
-}
+	if(flag == 0)
+		printf("NONE");
+	printf("\n");
+	return;}
